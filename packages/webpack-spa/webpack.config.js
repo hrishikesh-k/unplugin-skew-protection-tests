49f3f6a7578214609b9151079eee94fca1480d27
Hrishikesh Kokate <hrishi5200@gmail.com>
2026-09-08 00:42:31 +0530

    fiz webpack

--- packages/webpack-spa/webpack.config.js
+++ packages/webpack-spa/webpack.config.js
@@ -4,50 +4,62 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import skewProtection from '@netlify/unplugin-skew-protection/webpack'
 
-/** @type {import('webpack').Configuration} */
-export default {
-  devServer: {
-    static: './dist',
-    port: 5175,
-  },
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        exclude: /node_modules/,
-        test: /\.jsx?$/,
-        use: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-        ],
-      },
+/** @type {(env: unknown, argv: { mode?: string }) => import('webpack').Configuration} */
+export default (_env, argv) => {
+  const isProduction = argv.mode === 'production'
+
+  return {
+    devServer: {
+      static: './dist',
+      port: 5175,
+    },
+    entry: './src/index.js',
+    module: {
+      rules: [
+        {
+          exclude: /node_modules/,
+          test: /\.jsx?$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: 'defaults' }],
+                ['@babel/preset-react', { runtime: 'automatic', development: !isProduction }],
+              ],
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+          ],
+        },
+      ],
+    },
+    output: {
+      chunkFilename: '[name].[contenthash].chunk.js',
+      clean: true,
+      filename: '[name].[contenthash].js',
+      path: fileURLToPath(new URL('dist', import.meta.url)),
+      publicPath: '/',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        chunkFilename: '[name].[contenthash].chunk.css',
+        filename: '[name].[contenthash].css',
+      }),
+      skewProtection(),
     ],
-  },
-  output: {
-    chunkFilename: '[name].[contenthash].chunk.js',
-    clean: true,
-    filename: '[name].[contenthash].js',
-    path: fileURLToPath(new URL('dist', import.meta.url)),
-    publicPath: '/',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      chunkFilename: '[name].[contenthash].chunk.css',
-      filename: '[name].[contenthash].css',
-    }),
-    skewProtection(),
-  ],
-  resolve: {
-    extensions: [
-      '.js',
-      '.jsx',
-    ],
-  },
+    resolve: {
+      extensions: [
+        '.js',
+        '.jsx',
+      ],
+    },
+  }
 }
